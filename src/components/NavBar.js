f303c43b11a4a91633db1a6e276b5e1f142466b0
BrandomNV <noriegaverab@gmail.com>
2022-08-28 02:16:03 -0500

    navbar - menu despegable

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,32 +4,57 @@ import "./css/navbar.css";
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { Badge } from '@mui/material';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
 
 export const NavBar = () => {
-  return (
-    <nav className="container-navbar">
-        <div className="wrapper">
-            <div className="left">
-                <span className="lan">ES</span>
-                <div className="search-contain">
-                    <input className="search-input"/>
-                    <SearchIcon  className="search-icon"/>
+
+
+    const handleMenuDrop = () => {
+        const claseCSS = document.querySelector("#desplegable");
+        //console.log(claseCSS.classList.contains("nav-list-click-0"))
+        if(claseCSS.classList.contains("nav-list-click-0")) {
+            claseCSS.classList.replace("nav-list-click-0", "nav-list-click-1");
+        } else {
+            claseCSS.classList.replace("nav-list-click-1", "nav-list-click-0");
+        }
+    }
+
+
+    return (
+        <nav className="container-navbar">
+            <div className="wrapper">
+                <div className="left">
+                    <span className="lan">ES</span>
+                    <div className="search-contain">
+                        <input className="search-input"/>
+                        <SearchIcon  className="search-icon"/>
+                    </div>
+                </div>
+                <div className="center">
+                    <h1 className="logo">LLAMAshop.</h1>
+                </div>
+                <div className="right">
+                    <div>
+                        <ul className='nav-list-items'>
+                            <li className="menu-items">Register</li>
+                            <li className="menu-items">Login</li>
+                        </ul>
+                    </div>
+                    <div className="menu-items">
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlinedIcon className='shop-cart'/>
+                        </Badge>
+                    </div>
+                    <div className='menu-icon' onClick={handleMenuDrop}>
+                        <MenuOutlinedIcon />
+                    </div>
                 </div>
             </div>
-            <div className="center">
-                <h1 className="logo">LLAMAshop.</h1>
-            </div>
-            <div className="right">
-                <div className="menu-items">Register</div>
-                <div className="menu-items">Login</div>
-                <div className="menu-items">
-                    <Badge badgeContent={4} color="primary">
-                        <ShoppingCartOutlinedIcon className='shop-cart'/>
-                    </Badge>
-                </div>
-            </div>
-        </div>
-    </nav>
-  )
+            <ul className='nav-list-items-desplegable nav-list-click-0' id="desplegable">
+                <li>Register</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    )
 }
