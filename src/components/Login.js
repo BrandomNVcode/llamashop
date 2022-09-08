8b09df3b540e44a32d7069a052f34f1c6080019a
BrandomNV <noriegaverab@gmail.com>
2022-09-08 00:52:29 -0500

    login html css

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,13 +8,17 @@ export const Login = () => {
             <div className='img-logo-content'>
                 <div className='img-logo'></div>
             </div>
+
             <div className='login-form-content'>
+                <h1 className='title-llamashop'>LLAMAshop.</h1>
                 <form>
                     <label for="correo">Correo</label>
-                    <input type="email" name="correo" id="correo" value="usuario@demo.com" disabled/>
+                    <input type="email" name="correo" id="correo" value="usuario@demo.com" disabled className='input-box'/>
 
                     <label for="contracenia">Contraceña</label>
-                    <input type="password" name="correo" id="contracenia" value="test123" disabled/>
+                    <input type="password" name="correo" id="contracenia" value="test123" disabled className='input-box'/>
+
+                    <input type="submit" value="INICIAR SESIÓN" className='btn-login'/>
                 </form>
             </div>
         </div>
